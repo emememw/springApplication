fc14770308519c6fdc602314a54d1577fbc06c14
markus-wi <emveyh@googlemail.com>
2013-03-08 21:23:30 +0100

    added jsp test

--- src/main/java/com/markuswi/controller/WelcomeController.java
+++ src/main/java/com/markuswi/controller/WelcomeController.java
@@ -2,12 +2,14 @@ package com.markuswi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
 	
 	@RequestMapping(value = "/welcome")
-	public void home() {
-		System.out.println("test ... test ...");
+	public ModelAndView home() {
+		ModelAndView modelAndView = new ModelAndView("welcome");
+		return modelAndView;
 	}
 }
