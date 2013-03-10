0d0f3c850175a02424d1221c9710342a28096705
markus-wi <emveyh@googlemail.com>
2013-03-10 16:52:05 +0100

    modifed function / role / roleFunction structure

--- src/main/java/com/markuswi/role/Role.java
+++ src/main/java/com/markuswi/role/Role.java
@@ -1,9 +1,16 @@
 package com.markuswi.role;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.markuswi.rolefunction.RoleFunction;
 
 @Entity
 @Table
@@ -12,6 +19,9 @@ public class Role {
 	@Column
 	@Id
 	private String id;
+	
+	@OneToMany(mappedBy = "role", fetch = FetchType.LAZY)
+	private List<RoleFunction> roleFunction = new LinkedList<RoleFunction>();
 
 	public String getId() {
 		return id;
@@ -19,6 +29,14 @@ public class Role {
 
 	public void setId(String id) {
 		this.id = id;
+	}
+
+	public List<RoleFunction> getRoleFunction() {
+		return roleFunction;
+	}
+
+	public void setRoleFunction(List<RoleFunction> roleFunction) {
+		this.roleFunction = roleFunction;
 	}
 	
 	
